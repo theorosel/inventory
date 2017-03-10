708ae91ffe7be145460061701afcd2289603feaf
theorosel <theo.rosel@hetic.net>
2017-03-10 21:36:52 +0100

    :sparkles: Add Front on login page

--- includes/handle_login.php
+++ includes/handle_login.php
@@ -2,15 +2,15 @@
 
     $error_messages   = array();
 
+    // Variables declaration
     $username = '';
     $password = '';
 
-
+    // No error
     if (!empty($_POST)) {
 
         $username          = isset($_POST['username']) ? $_POST['username'] : '';
         $password          = isset($_POST['password']) ? $_POST['password'] : '';
-
 
 
         if (empty($username)) {
@@ -18,12 +18,13 @@
         }
 
         if (empty($password)) {
-            $error_messages['username'] = 'Please enter correct username and password';
+            $error_messages['password'] = 'Please enter correct username and password';
         }
 
 
         $password = hash('sha256',SALT.$_POST['password']);
 
+        // Users query
         $prepare = $db->prepare("SELECT * FROM users WHERE username =:username");
 
         $prepare->bindParam('username', $username);
@@ -31,18 +32,18 @@
 
         $result = $prepare->fetch();
 
+        // Password check
         if ($result && $result->password == $password) {
 
             $_SESSION['user'] = $username;
-
             $redirection = URL;
 
             header("location: $redirection");
             exit;
         }
 
+        // Error
         else {
-
             $error_messages['username'] = 'Please enter correct username and password';
         }
     }
--- views/pages/login.php
+++ views/pages/login.php
@@ -3,23 +3,45 @@
     include'includes/handle_login.php';
 ?>
 
-<div class="form-container">
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- $form -->
-    <form class="form" action="#" method="post" enctype="multipart/form-data">
-        <div class="form-control <?= array_key_exists('title', $error_messages) ? 'error' : '' ?> ">
-            <label for="title">Title :</label>
-            <input type="text" name="username" value="<?= $username ?>">
-        </div>
+        <title> <?= $title_page ?> </title>
 
-        <div class="form-control <?= array_key_exists('title', $error_messages) ? 'error' : '' ?> ">
-            <label for="title">Title :</label>
-            <input type="password" name="password">
-        </div>
+        <!-- $css import -->
+        <link href="<?= URL ?>assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="form-login">
+            <!-- $form container -->
+            <form class="form-login-container" action="#" method="post">
+                <div class="form-control-login <?= array_key_exists('username', $error_messages) ? 'error' : '' ?> ">
+                    <label for="username">Username :</label>
+                    <input type="text" name="username" value="<?= $username ?>" placeholder="root">
+                </div>
 
-        <!-- $submit ui component -->
-        <div class="submit add">
-            <input class="submit" type="submit" name="submit">
+                <div class="form-control-login <?= array_key_exists('password', $error_messages) ? 'error' : '' ?> ">
+                    <label for="password">Password :</label>
+                    <input type="password" name="password">
+                </div>
+
+                <!-- $error message -->
+                <div class="form-control-login">
+                    <div class="form-login-error ">
+                        <?php if (array_key_exists('username', $error_messages) || array_key_exists('password', $error_messages) ): ?>
+                            <p>Please enter correct username and password</p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+
+                <div class="form-control-login">
+                    <input  type="submit" name="Login" value="Log in">
+                </div>
+            </form>
         </div>
-    </form>
-</div>
+    </body>
+</html>
